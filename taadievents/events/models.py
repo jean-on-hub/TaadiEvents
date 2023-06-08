19a6956bb6635c48d38188d22fcba870e6f6a204
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2023-06-08 14:44:10 +0000

    we go

---
+++ taadievents/events/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+class Customers(models.model):
+    id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    
+class Event(models.Model):
+    customer_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    location = models.CharField(max_length=100)
+    picture_links = models.CharField(max_length=100)
+    customer_id = models.ForeignKey(Customers,on_delete= models.CASCADE)
