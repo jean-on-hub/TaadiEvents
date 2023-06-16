d1ceb5f6600043db6f8a611dcb0110ef1ccfd098
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2023-06-16 10:25:55 +0000

    d

--- taadievents/events/models.py
+++ taadievents/events/models.py
@@ -1,10 +1,9 @@
-from django.db import models
 
 # Create your models here.
 from django.db import models
-
+import uuid
 # Create your models here.
-class Customers(models.model):
+class Customers(models.Model):
     id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     
